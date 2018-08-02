ec9eceb3d604e2363920d30d7e935f0bb68c47bc
coreyhendrey <coreyhendrey@gmail.com>
2018-08-02 18:08:52 +1000

    Meaningful variable names
    Gave all variables meaningful names, and correct
    casing.
    LN -> lastName
    FN -> firstName
    EM -> emailAddress
    PN -> phoneNumber
    ID -> id
    FINES -> fines

--- src/Member.java
+++ src/Member.java
@@ -7,38 +7,38 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public class Member implements Serializable {
 
-	private String LN;
-	private String FN;
-	private String EM;
-	private int PN;
-	private int ID;
-	private double FINES;
+	private String lastName;
+	private String firstName;
+	private String emailAddress;
+	private int phoneNumber;
+	private int id;
+	private double fines;
 	
-	private Map<Integer, loan> LNS;
+	private Map<Integer, loan> loans;
 
 	
 	public Member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.LN = lastName;
-		this.FN = firstName;
-		this.EM = email;
-		this.PN = phoneNo;
-		this.ID = id;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.emailAddress = email;
+		this.phoneNumber = phoneNo;
+		this.id = id;
 		
-		this.LNS = new HashMap<>();
+		this.loans = new HashMap<>();
 	}
 
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Member:  ").append(ID).append("\n")
-		  .append("  Name:  ").append(LN).append(", ").append(FN).append("\n")
-		  .append("  Email: ").append(EM).append("\n")
-		  .append("  Phone: ").append(PN)
+		sb.append("Member:  ").append(id).append("\n")
+		  .append("  Name:  ").append(lastName).append(", ").append(firstName).append("\n")
+		  .append("  Email: ").append(emailAddress).append("\n")
+		  .append("  Phone: ").append(phoneNumber)
 		  .append("\n")
-		  .append(String.format("  Fines Owed :  $%.2f", FINES))
+		  .append(String.format("  Fines Owed :  $%.2f", fines))
 		  .append("\n");
 		
-		for (loan loan : LNS.values()) {
+		for (loan loan : loans.values()) {
 			sb.append(loan).append("\n");
 		}		  
 		return sb.toString();
@@ -46,28 +46,28 @@ public class Member implements Serializable {
 
 	
 	public int getId() {
-		return ID;
+		return id;
 	}
 
 	
 	public List<loan> getLoans() {
-		return new ArrayList<loan>(LNS.values());
+		return new ArrayList<loan>(loans.values());
 	}
 
 	
 	public int getNumberOfCurrentLoans() {
-		return LNS.size();
+		return loans.size();
 	}
 
 	
 	public double getFinesOwed() {
-		return FINES;
+		return fines;
 	}
 
 	
 	public void takeOutLoan(loan loan) {
-		if (!LNS.containsKey(loan.getId())) {
-			LNS.put(loan.getId(), loan);
+		if (!loans.containsKey(loan.getId())) {
+			loans.put(loan.getId(), loan);
 		}
 		else {
 			throw new RuntimeException("Duplicate loan added to member");
@@ -76,17 +76,17 @@ public class Member implements Serializable {
 
 	
 	public String getLastName() {
-		return LN;
+		return lastName;
 	}
 
 	
 	public String getFirstName() {
-		return FN;
+		return firstName;
 	}
 
 
 	public void addFine(double fine) {
-		FINES += fine;
+		fines += fine;
 	}
 	
 	public double payFine(double amount) {
@@ -94,20 +94,20 @@ public class Member implements Serializable {
 			throw new RuntimeException("Member.payFine: amount must be positive");
 		}
 		double change = 0;
-		if (amount > FINES) {
-			change = amount - FINES;
-			FINES = 0;
+		if (amount > fines) {
+			change = amount - fines;
+			fines = 0;
 		}
 		else {
-			FINES -= amount;
+			fines -= amount;
 		}
 		return change;
 	}
 
 
 	public void dischargeLoan(loan loan) {
-		if (LNS.containsKey(loan.getId())) {
-			LNS.remove(loan.getId());
+		if (loans.containsKey(loan.getId())) {
+			loans.remove(loan.getId());
 		}
 		else {
 			throw new RuntimeException("No such loan held by member");
